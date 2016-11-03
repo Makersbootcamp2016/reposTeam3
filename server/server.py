f4feaff7fa76a9a78d073122a0072688737f720e
Unknown <claire.barthe@edu.emlyon.com>
2016-11-03 17:46:22 +0100

    google maps test

--- server/server.py
+++ server/server.py
@@ -15,32 +15,28 @@ def home():
     import requests
     data = requests.get('https://api.smartcitizen.me/v0/devices/3740')
     resultat = data.json()
-    battery = round (resultat['data']['sensors'][1]['value'], 2)
-    humidity = round(resultat['data']['sensors'][2]['value'], 2)
-    temperature = round(resultat['data']['sensors'][3]['value'],2)
-    noise = round(resultat['data']['sensors'][7]['value'],2)
-    pollutionno2 = round(resultat['data']['sensors'][4]['value'],2)
-    pollutionco = round(resultat['data']['sensors'][5]['value'],2)
-    light = round(resultat['data']['sensors'][0]['value'],2)
+    battery = resultat['data']['sensors'][0]['value']
+    humidity = resultat['data']['sensors'][1]['value']
+    temperature = resultat['data']['sensors'][2]['value']
+    noise = resultat['data']['sensors'][6]['value']
+    pollutionno2 = resultat['data']['sensors'][3]['value']
+    pollutionco = resultat['data']['sensors'][4]['value']
+
+    lat = resultat ['data']['location']['latitude']['value']
+    longi = resultat ['data']['location']['longitude']['value']
+
     page = jinja_env.get_template('base.html')
-    return page.render(emplacement1=battery, emplacement2=humidity, emplacement3=temperature, emplacement4=noise, emplacement5=pollutionno2, emplacement6=pollutionco, emplacement7=light)
+    return page.render(emplacement1=battery, emplacement2=humidity, emplacement3=temperature, emplacement4=noise, emplacement5=pollutionno2, emplacement6=pollutionco, emplacement7=lat, emplacement8=longi)
 
 @app.route("/profiles")
 def profiles():
     page = jinja_env.get_template('profiles.html')
     return page.render()
 
-<<<<<<< Updated upstream
-@app.route("/maps.html")
+@app.route("/maps")
 def maps():
-
-    page = jinja_env.get_template('profiles.html')
-=======
-@app.route("/contacts")
-def contacts():
-    page = jinja_env.get_template('contacts.html')
->>>>>>> Stashed changes
+    page = jinja_env.get_template('localization.html')
     return page.render()
-
+    
 if __name__ == "__main__":
     app.run(port=10000)
